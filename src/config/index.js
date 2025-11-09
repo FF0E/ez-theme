5bc6417de59eccf5260acb2990a664b48b32f143
FF0ECCCC <173610639+ff0e@users.noreply.github.com>
2025-11-09 14:39:15 -0500

    update api

--- src/config/index.js
+++ src/config/index.js
@@ -29,7 +29,7 @@ export const config = {
         // 支持字符串形式(单个API地址)或数组形式(多个备选API地址)
         // 多个地址时，会按顺序检测可用性，并使用第一个可用的地址
         staticBaseUrl: [
-            'https://www.234981.xyz/api/v1'
+            'https://user-api.123972.xyz/api/v1'
         ],
 
         // 自动获取模式配置 (urlMode = 'auto'时使用)
